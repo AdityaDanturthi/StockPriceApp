cf87de6696469e4e419c5f8f97ff7254f640dab4
Aditya Danturthi <aditya2096@gmail.com>
2022-05-02 20:29:29 -0230

    Add files via upload

--- StockPriceApp.py
+++ StockPriceApp.py
@@ -1,10 +1,15 @@
 import yfinance as yf
 import streamlit as st
 import pandas as pd
+import numpy as np
+from datetime import datetime
 
 st.write("""
-Stock Price App
+# Stock Price App
+
+Shown are the ***closing price*** and ***volume*** of Amazon!
 """)
+
 
 tickerSymbol = 'AMZN'
 
@@ -12,5 +17,15 @@ tickerData = yf.Ticker(tickerSymbol)
 
 tickerDf =tickerData.history(period = '1d', start='2010-5-31', end= '2022-5-31')
 
+st.write("""
+## Closing price
+""")
+
 st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume
+""")
+
 st.line_chart(tickerDf.Volume)
+
